99fc597fdd549a0d4c4743f300b152213a90355a
Tanner Rogalsky <tanner@tannerrogalsky.com>
2015-05-23 23:23:39 -0400

    use BrowserSync for livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,23 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var babelify = require('babelify');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('default', ['drl', 'examples']);
-
-gulp.task('drl', function(){
-  var b = browserify({
-    entries: './drl.js',
+var browserifyDev = function(source) {
+  return browserify({
+    entries: source,
     debug: true,
     transform: [babelify.configure({
       jsxPragma: 'DRL.createElement'
     })]
   });
+}
+
+gulp.task('default', ['drl', 'examples']);
+
+gulp.task('drl', function(){
+  var b = browserifyDev('./drl.js');
 
   return b.bundle()
     .pipe(source('drl.js'))
@@ -22,13 +28,7 @@ gulp.task('drl', function(){
 });
 
 gulp.task('examples', function(){
-  var b = browserify({
-    entries: './examples/test.js',
-    debug: true,
-    transform: [babelify.configure({
-      jsxPragma: 'DRL.createElement'
-    })]
-  });
+  var b = browserifyDev('./examples/test.js');
 
   return b.bundle()
     .pipe(source('test.js'))
@@ -36,6 +36,15 @@ gulp.task('examples', function(){
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('watch', ['default'], function(){
-  gulp.watch(['./drl.js', './src/**/*.js', './examples/**/*.js'], ['default']);
+// watch files for changes and reload
+gulp.task('serve', ['default'], function() {
+  browserSync({
+    server: {
+      baseDir: './',
+      directory: true
+    }
+  });
+
+  gulp.watch(['./drl.js', './src/**/*.js'], ['drl', reload]);
+  gulp.watch(['./examples/**/*.js'], ['examples', reload]);
 });
